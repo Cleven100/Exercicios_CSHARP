c24aab3dd99f44b3a70eed58a53865eb72991d9c
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-03-19 21:55:35 -0300

    19/03

---
+++ Exercícios_ValueType_ReferenceTypes/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int.TryParse(Console.ReadLine(), out int n);
+            int soma = 0, a;
+            a = n + 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n)
+                {
+                    break;
+                }
+                if (n % i == 0)
+                {
+                    soma += i;
+                }
+
+
+            }
+            
+
+            if (n == soma)
+            {
+                Console.WriteLine($"{n} eh perfeito");
+                Console.ReadLine();
+            }
+            if (n != soma)
+            {
+                Console.WriteLine($"{n} nao eh perfeito");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
